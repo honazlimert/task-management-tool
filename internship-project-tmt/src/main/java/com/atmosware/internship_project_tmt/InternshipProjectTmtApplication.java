5f3199536e82109edba46ba11edaad0b1d3ff262
Mert Honazlı <honazlimert@hotmail.com>
2026-08-14 09:38:50 +0300

    Initialize project structure and database connection

---
+++ internship-project-tmt/src/main/java/com/atmosware/internship_project_tmt/InternshipProjectTmtApplication.java
@@ -0,0 +1,13 @@
+package com.atmosware.internship_project_tmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProjectTmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProjectTmtApplication.class, args);
+	}
+
+}
---
+++ internship-project-tmt/src/test/java/com/atmosware/internship_project_tmt/InternshipProjectTmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmosware.internship_project_tmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipProjectTmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
